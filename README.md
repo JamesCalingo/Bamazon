08f99e9963b4f636889bbbbc0c9085af0bc538b3
JC <james.calingo@gmail.com>
2019-04-30 19:06:43 -0400

    now with YouTube demo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bamazon
-Bamazon - a node application that uses a MySQL database to simulate making transactions on Amazon.com
+Bamazon - a node application that uses a MySQL database to simulate making transactions on online stores like Amazon
+Video demo: https://youtu.be/lFj998ibiv0
---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (productName, department, price, stockQuantity)
+VALUES
+("Atari Jaguar", "Electronics", 99.99, 75),
+("Blank t-shirt", "Clothing", 10.00, 10000),
+("Cat Catan", "Toys and Games", 49.99, 3000),
+("Official Super Smash Bros. Beastball", "Sports", 19.99, 2500),
+("Laundry Basket", "Misc", 23.00, 2500),
+("I Am America (And So Can You!)", "Books", 19.99, 500),
+("Potato Salad", "Food", 5.99, 250000)
+("A Sports Team", "Sports", 5000000.00, 3);
